67b5acf7269bc7d459da8609f36649b03232e84c
Daniel Augusto Bonaldo <dabonaldo@stefanini.com>
2015-12-03 13:49:51 -0200

    iniciado a funcionalidade de quantidade de gols

---
+++ app/modules/goals-by-team.js
@@ -0,0 +1,32 @@
+var MongoClient = require('mongodb').MongoClient;
+
+module.exports = function(callback) {
+
+    var gameList = [];
+
+    MongoClient.connect('mongodb://127.0.0.1:27017/test',
+        function(erro, db) {
+            if (erro) throw err;
+            db.collection('resultados').aggregate([{
+                $group: {
+                    _id: {
+                        qtGoals: {
+                            $add: ['$score1', '$score2']
+                        }
+                    },
+                    count: {
+                        $sum: 1
+                    }
+                }
+            }, {
+                $sort: {
+                    count: -1
+                }
+            }], (function(err, docs) {
+                if (erro) throw err;
+                callback(docs);
+                db.close();
+            }));
+        });
+
+};
--- app/modules/mongodb_commands.js
+++ app/modules/mongodb_commands.js
@@ -30,3 +30,49 @@ db.resultados.aggregate([{
         count: -1
     }
 }]);
+
+// gols em casa 
+db.resultados.aggregate([{
+    $group: {
+        _id: '$team1.name',
+        GF: {
+            $sum: '$score1'
+        },
+        GA: {
+            $sum: '$score2'
+        },
+        GD: {
+            $sum: {
+                $subtract: ['$score1', '$score2']
+            }
+        }
+
+    }
+}, {
+    $sort: {
+        GD: -1
+    }
+}]);
+
+// gols fora
+db.resultados.aggregate([{
+    $group: {
+        _id: '$team2.name',
+        GF: {
+            $sum: '$score2'
+        },
+        GA: {
+            $sum: '$score1'
+        },
+        GD: {
+            $sum: {
+                $subtract: ['$score2', '$score1']
+            }
+        }
+
+    }
+}, {
+    $sort: {
+        GD: -1
+    }
+}]);
